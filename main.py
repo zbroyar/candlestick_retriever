701221961ba338557b035e55ddcb366b98e263eb
Mykola Stryebkov <mykola.stryebkov@icloud.com>
2021-05-26 20:06:11 +0300

    remove parquet, add read data since the last data ts

--- main.py
+++ main.py
@@ -54,16 +54,6 @@ METADATA = {
     'data': []
 }
 
-def write_metadata(n_count):
-    """Write the metadata file dynamically so we can include a pair count."""
-
-    METADATA['subtitle'] = f'1 minute candlesticks for all {n_count} cryptocurrency pairs'
-    METADATA['description'] = f"""### Introduction\n\nThis is a collection of all 1 minute candlesticks of all cryptocurrency pairs on [Binance.com](https://binance.com). All {n_count} of them are included. Both retrieval and uploading the data is fully automated—see [this GitHub repo](https://github.com/gosuto-ai/candlestick_retriever).\n\n### Content\n\nFor every trading pair, the following fields from [Binance's official API endpoint for historical candlestick data](https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md#klinecandlestick-data) are saved into a Parquet file:\n\n```\n #   Column                        Dtype         \n---  ------                        -----         \n 0   open_time                     datetime64[ns]\n 1   open                          float32       \n 2   high                          float32       \n 3   low                           float32       \n 4   close                         float32       \n 5   volume                        float32       \n 6   quote_asset_volume            float32       \n 7   number_of_trades              uint16        \n 8   taker_buy_base_asset_volume   float32       \n 9   taker_buy_quote_asset_volume  float32       \ndtypes: datetime64[ns](1), float32(8), uint16(1)\n```\n\nThe dataframe is indexed by `open_time` and sorted from oldest to newest. The first row starts at the first timestamp available on the exchange, which is July 2017 for the longest running pairs.\n\nHere are two simple plots based on a single file; one of the opening price with an added indicator (MA50) and one of the volume and number of trades:\n\n![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F2234678%2Fb8664e6f26dc84e9a40d5a3d915c9640%2Fdownload.png?generation=1582053879538546&alt=media)\n![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F2234678%2Fcd04ed586b08c1576a7b67d163ad9889%2Fdownload-1.png?generation=1582053899082078&alt=media)\n\n### Inspiration\n\nOne obvious use-case for this data could be technical analysis by adding indicators such as moving averages, MACD, RSI, etc. Other approaches could include backtesting trading algorithms or computing arbitrage potential with other exchanges.\n\n### License\n\nThis data is being collected automatically from crypto exchange Binance."""
-
-    with open('compressed/dataset-metadata.json', 'w') as file:
-        json.dump(METADATA, file, indent=4)
-
-
 def get_batch(symbol, interval='1m', start_time=0, limit=1000):
     """Use a GET request to retrieve a batch of candlesticks. Process the JSON into a pandas
     dataframe and return it. If not successful, return an empty dataframe.
@@ -113,13 +103,17 @@ def all_candles_to_csv(base, quote, interval='1m'):
         last_timestamp = 0
     old_lines = len(batches[-1].index)
 
+    now = (time.time())*1000
+    last_timestamp = last_timestamp
+    # print(f'{base}--{quote} last timestamp: {last_timestamp}. Now: {now}')
+
     # gather all candlesticks available, starting from the last timestamp loaded from disk or 0
     # stop if the timestamp that comes back from the api is the same as the last one
     previous_timestamp = None
 
     while previous_timestamp != last_timestamp:
         # stop if we reached data from today
-        if date.fromtimestamp(last_timestamp / 1000) >= date.today():
+        if last_timestamp >= now:
             break
 
         previous_timestamp = last_timestamp
@@ -149,18 +143,9 @@ def all_candles_to_csv(base, quote, interval='1m'):
         print(datetime.now(), base, quote, interval, str(last_datetime)+covering_spaces, end='\r', flush=True)
 
     # write clean version of csv to parquet
-    parquet_name = f'{base}-{quote}.parquet'
-    full_path = f'compressed/{parquet_name}'
     df = pd.concat(batches, ignore_index=True)
     df = pp.quick_clean(df)
-    pp.write_raw_to_parquet(df, full_path)
-    METADATA['data'].append({
-        'description': f'All trade history for the pair {base} and {quote} at 1 minute intervals. Counts {df.index.size} records.',
-        'name': parquet_name,
-        'totalBytes': os.stat(full_path).st_size,
-        'columns': []
-    })
-
+ 
     # in the case that new data was gathered write it to disk
     if len(batches) > 1:
         df.to_csv(f'data/{base}-{quote}.csv', index=False)
@@ -188,20 +173,26 @@ def main():
     n_count = len(all_pairs)
     for n, pair in enumerate(all_pairs, 1):
         base, quote = pair
-        new_lines = all_candles_to_csv(base=base, quote=quote)
-        if new_lines > 0:
-            print(f'{datetime.now()} {n}/{n_count} Wrote {new_lines} new lines to file for {base}-{quote}')
-        else:
-            print(f'{datetime.now()} {n}/{n_count} Already up to date with {base}-{quote}')
+        bf, qf = False, False
+
+        if base == "BTC" or base == "USDT":
+            bf = True
+
+        if quote == "BTC" or quote == "USDT":
+            qf = True
+
+        if bf or qf:
+            new_lines = all_candles_to_csv(base=base, quote=quote)
+            if new_lines > 0:
+                print(f'{datetime.now()} {n}/{n_count} Wrote {new_lines} new lines to file for {base}-{quote}')
+        #else:
+        #    print(f'Ignoring {base}-{quote}')
 
     # clean the data folder and upload a new version of the dataset to kaggle
     try:
         os.remove('compressed/.DS_Store')
     except FileNotFoundError:
         pass
-    write_metadata(n_count)
-    yesterday = date.today() - timedelta(days=1)
-    subprocess.run(['kaggle', 'datasets', 'version', '-p', 'compressed/', '-m', f'full update of all {n_count} pairs up to {str(yesterday)}'])
     os.remove('compressed/dataset-metadata.json')
 
 
